72f6509ddaa264c68c57d5c23532a76f7a7aeb76
vyas-git <vyasreddy.tech@gmail.com>
2024-05-13 03:32:03 +0530

    logger fixed

--- wordpress-operator/cmd/main.go
+++ wordpress-operator/cmd/main.go
@@ -124,6 +124,7 @@ func main() {
 
 	if err = (&controllers.WordpressReconciler{
 		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Wordpress"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Wordpress")
